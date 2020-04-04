36a06e8bd28c90b0704bf18e7d53b74958da49b1
Lincoln de Sousa <lincoln@comum.org>
2020-04-04 00:43:53 -0400

    Make a teeny website of the scraped content

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import requests
 import os
 import io
+import jinja2
 
 from bs4 import BeautifulSoup
 
@@ -11,12 +12,15 @@ ROOT_URL = 'http://www.overmundo.com.br'
 INITIAL_PATH = '/arquivo_usuario/daniel-duende'
 
 
+# ---- Teeny lil silly cache system ----
+
+
 def random_sleep():
     time.sleep(random.randint(15, 20))
 
 
 def get_page(url):
-    # random_sleep()
+    random_sleep()
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
     }
@@ -32,7 +36,7 @@ def savefile(name, content):
 
 
 def gen_file_name(url):
-    return './cache-' + url.replace('/', '-').replace(':', '-')
+    return './cache/cache-' + url.replace('/', '-').replace(':', '-')
 
 
 def cachedurl(url):
@@ -43,42 +47,80 @@ def cachedurl(url):
         return False, savefile(name, get_page(url))
 
 
+# ---- Templatery ----
+
+
+TMPL_OUT = os.path.join(os.path.dirname(__file__), 'site')
+TMPL_PATH = os.path.join(os.path.dirname(__file__), 'tmpl')
+TMPL_ENV = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(TMPL_PATH),
+    autoescape=jinja2.select_autoescape(['html', 'xml']))
+
+
+def do_template(tmpl, name=None, **kwargs):
+    output = os.path.join(TMPL_OUT, name or tmpl)
+    template = TMPL_ENV.get_template(tmpl)
+    io.open(output, 'w').write(template.render(**kwargs))
+
+
+# ---- Download & Process Content ---
+
+
 def download_path(path):
     url = f"{ROOT_URL}{path}"
     _, content = cachedurl(url)
     return content
 
 
-def scrape_page(page):
-    print(page.select_one('h1').text)
-    print(page.select_one('div#conteudo div.conteudo').text)
+def scrape_page(path, page):
+    title = page.select_one('h1').text
+    date = page.select_one('.extra').text.strip().split('\r')[0]
+    content = page.select_one('div#conteudo div.conteudo')
+    del content['class']
+    do_template(
+        'page.html',
+        page_path(path),
+        title=title,
+        date=date,
+        content=content)
 
 
-def scrape_doc(page):
-    print(page.select_one('h1').text)
+def scrape_doc(path, page):
+    title = page.select_one('h1')
+    date = page.select_one('.extra').text.strip().split('\r')[0]
     download_link = page.select_one('div.botao.baixar a')
     download_path(download_link.get('href'))
-    text_object = (
+    content = (
         page.select_one('div#conteudo div.obraTexto') or
         page.select_one('div#conteudo div.conteudo'))
-    print(text_object.text)
 
 
-
-def parse_page(path):
+def do_page(path):
     content = download_path(path)
     page = BeautifulSoup(content, 'html.parser')
     if path.startswith('/banco'):
-        scrape_doc(page)
+        scrape_doc(path, page)
     else:
-        scrape_page(page)
+        scrape_page(path, page)
 
 
-def parse_root():
-    page = BeautifulSoup(download_path(INITIAL_PATH), 'html.parser')
-    for link in page.select('div[class=colaboracoes] h3 a'):
-        parse_page(link.get('href'))
+def page_path(path):
+    return f"{path.split('/')[-1]}.html"
+
+
+def page_attrs(page_url, page_title):
+    return {"url": page_path(page_url), "title": page_title}
+
+
+def do_root():
+    root_page = BeautifulSoup(download_path(INITIAL_PATH), 'html.parser')
+    pages = []
+    for link in root_page.select('div[class=colaboracoes] h3 a'):
+        page_url = link.get('href')
+        do_page(page_url)
+        pages.append(page_attrs(page_url, link.text))
+    do_template('index.html', pages=pages)
 
 
 if __name__ == '__main__':
-    parse_root()
+    do_root()
